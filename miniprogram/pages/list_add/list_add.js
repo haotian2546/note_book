a0d734924adb64f7f075963b7f1da8773fb6745b
xyh <xuyonghao0806@163.com>
2019-09-10 20:24:50 +0800

    s

--- miniprogram/pages/list_add/list_add.js
+++ miniprogram/pages/list_add/list_add.js
@@ -1,65 +1,69 @@
 // pages/list_add/list_add.js
+const app = getApp();
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    num: 0,
+    des: '',
+    id: '',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-    console.log(options);
+    this.setData({
+      id: options.id
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  handel_add: function () {
+    if (this.data.num > 0 && !isNan(this.data.num) && this.data.id && this.data.des) {
+      let data = {
+        num: this.data.num,
+        des: this.data.des,
+        author: app.globalData.userInfo._id,
+        create_time: (new Date()).getTime(),
+        change_time: (new Date()).getTime(),
+        list: this.data.id,
+      }
+      db.collection('list_note').add({ data }).then(res => {
+        console.log(res);
+      })
+    } else {
+      wx.showToast({
+        title: '好像有点问题！！！',
+        icon: 'none',
+        duration: 1500,
+      });
+    }
 
   },
+  handel_num: function (e) {
+    this.setData({
+      num: e.detail.value
+    })
+  },
+  handel_des: function (e) {
+    this.setData({
+      des: e.detail.value
+    })
+  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+
+
+
+
+
+
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
